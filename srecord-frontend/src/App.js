9286c5d3db641eb104fb75f49a1e2c161ee88554
cuzondu25 <cuzondu25@gmail.com>
2024-06-27 16:47:37 +0100

    SRecord application with node-moduls

--- srecord-frontend/src/App.js
+++ srecord-frontend/src/App.js
@@ -1,31 +1,41 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import SalesEntryForm from './components/SalesEntryForm';
 import WeeklyReport from './components/WeeklyReport';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Sales Entry</Link>
-            </li>
-            <li>
-              <Link to="/weekly-report">Weekly Report</Link>
-            </li>
-          </ul>
-        </nav>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>SRecord</h1>
+            </header>
+            <main>
+                <Router>
+                    <div >
+                        <nav className="navbar">
+                            <ul className="nav-tabs">
+	                        <li className="nav-tab">
+	                            <Link to="/">Home</Link>
+	                        </li>
+                                <li className="nav-tab">
+                                    <Link to="/sales-entry">Sales Entry</Link>
+                                </li>
+                                <li className="nav-tab">
+                                    <Link to="/weekly-report">Weekly Report</Link>
+                                </li>
+                            </ul>
+                        </nav>
 
-        <Routes>
-          <Route path="/" exact component={SalesEntryForm} />
-          <Route path="/weekly-report" component={WeeklyReport} />
-        </Routes>
-      </div>
-    </Router>
-  );
+                        <Routes>
+                            <Route path="/sales-entry" element={<SalesEntryForm />} />
+                            <Route path="/weekly-report" element={<WeeklyReport />} />
+                        </Routes>
+                    </div>
+                </Router>
+            </main>
+        </div>
+    );
 }
 
 export default App;
